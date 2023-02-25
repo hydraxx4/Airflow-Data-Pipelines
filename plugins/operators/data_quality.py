a1b106b318706a15a1b96fcd40829eaed082e0d1
Trung Tran <99697304+hydraxx4@users.noreply.github.com>
2023-02-25 15:36:27 -0800

    Add files via upload

--- plugins/operators/data_quality.py
+++ plugins/operators/data_quality.py
@@ -9,8 +9,8 @@ class DataQualityOperator(BaseOperator):
     @apply_defaults
     def __init__(self,
                  # Define  operators params (with defaults) 
-                 redshift_conn_id = '',
-                 data_quality_check = '',
+                 redshift_conn_id = '', # Connection ID for the Redshift cluster
+                 data_quality_check = [], # Description of the data quality check being performed
                  *args, **kwargs):
 
         super(DataQualityOperator, self).__init__(*args, **kwargs)
@@ -19,39 +19,28 @@ class DataQualityOperator(BaseOperator):
         self.data_quality_check = data_quality_check
 
     def execute(self, context):
-        # Log that the operator is starting
-        self.log.info('S3ToRedshiftOperator is starting...')
+        self.log.info('Starting the S3ToRedshiftOperator...')  # Log that the task is starting
 
-        redshift_hook = PostgresHook(self.redshift_conn_id)
+        redshift_hook = PostgresHook(self.redshift_conn_id)  # Create a hook to connect to the Redshift cluster
 
-        def check_greater_than_zero(*args, **kwargs):
+        def check_greater_than_zero(*args, **kwargs):  # Define a helper function for the data quality check
             table = kwargs["params"]["table"]
-            redshift_hook = PostgresHook("redshift")
+            # Get the number of records in the table from the Redshift cluster
             records = redshift_hook.get_records(f"SELECT COUNT(*) FROM {table}")
             if len(records) < 1 or len(records[0]) < 1:
-                raise ValueError(f"Data quality check failed. {table} returned no results")
+                raise ValueError(f"No results were returned by {table}. Data quality check failed.")
             num_records = records[0][0]
             if num_records < 1:
-                raise ValueError(f"Data quality check failed. {table} contained 0 rows")
-            logging.info(f"Data quality on table {table} check passed with {records[0][0]} records")
+                raise ValueError(f"{table} contains 0 rows. Data quality check failed.")
+            logging.info(f"Data quality check on {table} passed with {records[0][0]} records.")
+
+        # Call the helper function for each table to be checked
+        check_songplays_task = check_greater_than_zero(params={'table': 'songplays'})
+        check_songs_task = check_greater_than_zero(params={'table': 'songs'})
+        check_artists_task = check_greater_than_zero(params={'table': 'artists'})
+
         
-        check_songplays_task = check_greater_than_zero(
-            params={
-                'table':'songplays'
-                }
-        ) 
 
-        check_songs_task = check_greater_than_zero(
-            params={
-                'table':'songs'
-                }
-        )  
-
-        check_artists_task = check_greater_than_zero(
-            params={
-                'table':'artists'
-                }
-        )  
 
 
         
